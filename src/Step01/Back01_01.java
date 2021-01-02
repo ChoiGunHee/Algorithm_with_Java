efb3521313d0c3930e99fb74d2787c2b5bdce980
GunHee Choi <godrjsmgl@gmail.com>
2021-01-02 20:19:19 +0000

    Solve Backjoon Step01 01_01 with Java

---
+++ src/Step01/Back01_01.java
@@ -0,0 +1,18 @@
+/**
+* 2021. 01. 02
+* Creater : Gunhee Choi
+* Problem Number : 2557
+* Title : Hello World
+* Problem :
+	Hello World!를 출력하시오.
+* Input : None
+* Output : Hello World!
+**/
+
+package Step01;
+
+public class Back01_01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
