25d54ed7c1d3589cd92e290c223a1a099cad52ee
GunHee Choi <godrjsmgl@gmail.com>
2021-01-04 20:19:19 +0000

    Solve Backjoon Step01 01_04 with Java

---
+++ src/Step01/Back01_03.java
@@ -0,0 +1,27 @@
+/**
+* 2021. 01. 04
+* Creater : Gunhee Choi
+* Problem Number : 	10171
+* Title : 고양이
+* Problem :
+	아래 예제와 같이 고양이를 출력하시오.
+* Input : None
+* Output :
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ 
+**/
+
+
+package Step01;
+
+public class Back01_03 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
