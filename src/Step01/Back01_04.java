2201cb8c77c66736124fbb4c5f217083b9688250
GunHee Choi <godrjsmgl@gmail.com>
2021-01-05 20:19:19 +0000

    Solve Backjoon Step01 01_05 with Java

---
+++ src/Step01/Back01_04.java
@@ -0,0 +1,28 @@
+/**
+* 2021. 01. 05
+* Creater : Gunhee Choi
+* Problem Number : 	10172
+* Title : 개
+* Problem :
+	아래 예제와 같이 개를 출력하시오.
+* Input : None
+* Output :
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+ 
+**/
+
+package Step01;
+
+public class Back01_04 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
