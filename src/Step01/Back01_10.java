ca30bdd8e5db68d0755132f3e1c562b7718ca8d2
GunHee Choi <godrjsmgl@gmail.com>
2021-01-11 22:19:19 +0000

    Sovle Backjoon Step01 01_10 with Java

---
+++ src/Step01/Back01_10.java
@@ -0,0 +1,36 @@
+/**
+* 2021. 01. 11
+* Creater : Gunhee Choi
+* Problem Number : 	10430
+* Title : 나머지
+* Problem :
+(A+B)%C는 ((A%C) + (B%C))%C 와 같을까?
+(A×B)%C는 ((A%C) × (B%C))%C 와 같을까?
+세 수 A, B, C가 주어졌을 때, 위의 네 가지 값을 구하는 프로그램을 작성하시오.
+* Input : 첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+	5 8 4
+* Output :
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+	1
+	1
+	0
+	0
+**/
+
+package Step01;
+import java.util.*;
+
+public class Back01_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        sc.close();
+         
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B%C))%C);
+    }
+}
