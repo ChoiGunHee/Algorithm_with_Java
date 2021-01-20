de5225c63e7305ea69bf6bb86e6ba107175d865a
GunHee Choi <godrjsmgl@gmail.com>
2021-01-20 22:52:19 +0000

    Solve Backjoon Step02_03 with Java

--- src/Step02/Back02_03.java
+++ src/Step02/Back02_03.java
@@ -19,6 +19,18 @@ import java.util.*;
 
 public class Back02_03 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        sc.close();
+        
+        if( year%4 == 0 )  {
+		    if( (year%100 != 0) | (year%400 == 0)) {
+			    System.out.println("1");
+		    } else {
+			    System.out.println("0");
+		    }
+	    } else {
+		    System.out.println("0");
+	    }
     }
 }
