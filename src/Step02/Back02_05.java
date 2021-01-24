00caef19cc621c29fe38b418f8e16d60b17b457d
GunHee Choi <godrjsmgl@gmail.com>
2021-01-24 15:35:19 +0000

    Solve Backjoon Step02 02_05 with Java

--- src/Step02/Back02_05.java
+++ src/Step02/Back02_05.java
@@ -23,6 +23,21 @@ import java.util.*;
 
 public class Back02_05 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        sc.close();
+        
+        M = M - 45;
+        if(M < 0) {
+            H = H - 1;
+            M = M + 60;
+        }
 
+        if( H < 0 ) {
+            H = H + 24;
+        }
+
+        System.out.printf("%d %d\n", H, M);
     }
 }
