c59ba82bd208eed8a7b943cb30556eeef311e17d
GunHee Choi <godrjsmgl@gmail.com>
2021-01-26 22:45:19 +0000

    Solve Backjoon Step03 03_01 with Java

--- src/Step03/Back03_01.java
+++ src/Step03/Back03_01.java
@@ -25,6 +25,11 @@ import java.util.*;
 
 public class Back03_01 {
     public static void main(String[] args) {
-    
+        Scanner sc = new Scanner(System.in);
+        int dan = sc.nextInt();
+        sc.close();
+        
+        for(int i=1; i<=9; i++)
+            System.out.printf("%d * %d = %d\n", dan, i, dan*i);
     }
 }
