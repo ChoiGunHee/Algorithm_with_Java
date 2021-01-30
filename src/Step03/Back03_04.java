34b56dfe52714f098079b57677a42dc0cbde0e18
GunHee Choi <godrjsmgl@gmail.com>
2021-01-30 13:19:19 +0000

    Solve Backjoon Step03 03_04 with Java

--- src/Step03/Back03_04.java
+++ src/Step03/Back03_04.java
@@ -34,14 +34,14 @@ import java.util.*;
 import java.io.*;
 
 public class Back03_04 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
 		
         StringTokenizer st;
-        
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine()," ");
             bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
